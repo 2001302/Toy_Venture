1ccceb8d9710ebb659420016758881b509ee29f4
kyo <yoonjunkyo@gmail.com>
2024-11-25 21:12:43 +0900

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { HeroSection } from "@/components/hero-section"
 import { FeaturesSection } from "@/components/features-section"
 
-export default async function Home() {
+export default async function Page() {
   return (
     <div className="flex flex-col">
-      {/* @ts-expect-error Async Server Component */}
       <HeroSection />
       <FeaturesSection />
     </div>
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      };
+    }
+    return config;
+  },
 }
 
 export default nextConfig
